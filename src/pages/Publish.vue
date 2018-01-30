f1750e94289f39dcb1235e9c5a76a38a04e3a314
lenconda <1053464288@qq.com>
2018-01-30 17:35:20 +0800

    即将完成发布动态页面

--- src/pages/Publish.vue
+++ src/pages/Publish.vue
@@ -26,8 +26,45 @@
       <div class="right-buttons"><button class="form-control-btn" @click="uploadImg">发布</button></div>
     </div>
     <van-popup position="bottom" v-model="showStatus" class="select-status">
-      <div class="confirms"></div>
+      <div class="confirms">
+        <ul>
+          <li class="cancel-btn" @click="actions(1)">取消</li>
+          <li class="confirm-btn" @click="actions(1)">确定</li>
+        </ul>
+      </div>
       <van-picker :columns="status" @change="onStatusChange" />
+    </van-popup>
+    <van-popup position="bottom" v-model="showTags" class="select-tags">
+      <div class="confirms">
+        <ul>
+          <li class="cancel-btn" @click="actions(0)">取消</li>
+          <li class="confirm-title">选择标签</li>
+          <li class="confirm-btn" @click="actions(0)">确定</li>
+        </ul>
+      </div>
+      <div class="tags-wrapper">
+        <div class="tag-list">
+          <label class="tag-wrapper">
+            <input type="checkbox" v-model="selectedTags" value="萌宠">
+            <span class="tag-content">萌宠</span>
+          </label>
+          <label class="tag-wrapper">
+            <input type="checkbox" v-model="selectedTags" value="狗年大吉">
+            <span class="tag-content">狗年大吉</span>
+          </label>
+          <label class="tag-wrapper">
+            <input type="checkbox" v-model="selectedTags" value="喵星人">
+            <span class="tag-content">喵星人</span>
+          </label>
+          <label class="tag-wrapper">
+            <input type="checkbox" v-model="selectedTags" value="铲屎官">
+            <span class="tag-content">铲屎官</span>
+          </label>
+        </div>
+        <div class="refresh-wrapper">
+          <i class="iconfont ptsh-refresh refresh-btn"></i>
+        </div>
+      </div>
     </van-popup>
   </div>
 </template>
@@ -61,7 +98,8 @@ export default {
       selectedTags: [],
       showTags: false,
       showStatus: false,
-      status: ["开心", "难过", "怀孕", "生病"]
+      status: ["开心", "难过", "怀孕", "生病"],
+      currentStatus: ''
     }
   },
   methods: {
@@ -72,15 +110,18 @@ export default {
       switch (option) {
         case 0:
           this.showTags = !this.showTags
-          break;
+          this.showStatus = false
+          break
         case 1:
           this.showStatus = !this.showStatus
+          this.showTags = false
+          break
         default:
-          break;
+          break
       }
     },
     onStatusChange (picker, value, index) {
-
+      this.currentStatus = value
     },
     selectImg () {
       document.getElementById('upfile').click()
@@ -196,7 +237,7 @@ export default {
   background-size: 100%;
   width: 25px;
   height: 25.5px;
-  left: 89px;
+  left: 88px;
   top: 15.5px;
 }
 .left-buttons > i:last-child {
@@ -204,7 +245,7 @@ export default {
   background-size: 100%;
   width: 29.5px;
   height: 26.5px;
-  left: 147.5px;
+  left: 149px;
   top: 15px;
 }
 .left-buttons > i:first-child:active {
@@ -305,5 +346,84 @@ export default {
   width: 100%;
   height: 44px;
   background: #fff;
+  position: relative;
+  border-bottom: 1px solid #ccc;
+}
+.confirms > ul > li {
+  font-size: 16px;
+  width: auto;
+  display: inline-block;
+}
+.confirms > ul > li.confirm-btn {
+  color: #1d5faa;
+  float: right;
+  padding: 11px 13px;
+}
+.confirms > ul > li.cancel-btn {
+  float: left;
+  color: #000;
+  padding: 11px 13px;
+}
+.confirms > ul > li.confirm-title {
+  font-size: 18px;
+  color: #000;
+  padding: 9px 0;
+  width: calc(100% - 58 * 2);
+}
+.select-tags {
+  width: 100%;
+  height: 319px;
+  background-color: #fff;
+}
+.tags-wrapper {
+  width: 100%;
+  height: 220px;
+  background-color: #f5f5f5;
+}
+div.tags-wrapper div.tag-list {
+  width: 346px;
+  height: 143px;
+  overflow-y: auto;
+  margin: 0 auto;
+  padding-top: 17px;
+  text-align: left;
+}
+div.tags-wrapper div.refresh-wrapper {
+  width: 100%;
+  height: 60px;
+}
+.refresh-btn {
+  font-size: 26.5px;
+  line-height: 60px;
+  display: inline-block;
+}
+.refresh-btn:active {
+  color: #686868;
+}
+/* Tags */
+input[type=checkbox] {
+  display: none;
+}
+label.tag-wrapper {
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px 10px 0 10px;
+}
+span.tag-content {
+  text-align: center;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  padding: 6px;
+  margin: 0 2.5px;
+  border-radius: 4px;
+  background-color: #fff;
+  border: 1px solid #949494;
+  transition: all .4s;
+}
+input:checked + span {
+  color: #2cbce7;
+  border-color: #2cbce7;
+  transition: all .4s;
 }
 </style>
