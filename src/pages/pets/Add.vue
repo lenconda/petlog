b5940edf16468ccd4b3aab74349ef0cd8b175e20
lenconda <1053464288@qq.com>
2018-02-20 19:54:44 +0800

    添加宠物功能

--- src/pages/pets/Add.vue
+++ src/pages/pets/Add.vue
@@ -9,39 +9,74 @@
     <div class="form-wrapper">
       <div class="input-group">
         <span>宠物昵称</span>
-        <input type="text" placeholder="设置昵称" maxlength="16">
+        <input type="text" placeholder="设置昵称" maxlength="16" v-model="nickname">
       </div>
       <div class="input-group">
         <span>宠物简介</span>
-        <input type="text" placeholder="填写" maxlength="16">
+        <input type="text" placeholder="填写" maxlength="16" v-model="motto">
       </div>
       <div class="input-group">
         <span>宠物性别</span>
         <div class="gender">
           <label>
-            <input type="radio" value="female">
+            <input type="radio" value="female" v-model="gender">
             <span class="gender_female"></span><span>母</span>
           </label>
           <label>
-            <input type="radio" value="male">
+            <input type="radio" value="male" v-model="gender">
             <span class="gender_male"></span><span>公</span>
           </label>
         </div>
       </div>
-      <div class="input-group">
-        <span>所在地</span>
-        <span class="position">&nbsp;<i class="right"></i></span>
+      <div class="input-group" @click="birth = true">
+        <span>出生日期</span>
+        <span class="position">{{ birthDay == '' ? '选择宠物生日' : birthDay }}&nbsp;<i class="right"></i></span>
+      </div>
+      <div class="input-group" @click="meet = true">
+        <span>相遇日期</span>
+        <span class="position">{{ meetDay == '' ? '选择相遇日期' : meetDay }}&nbsp;<i class="right"></i></span>
+      </div>
+      <div class="input-group" @click="showVariety = true">
+        <span>宠物品种</span>
+        <span class="position">{{ variety == '' ? '选择宠物品种' : variety }}&nbsp;<i class="right"></i></span>
       </div>
     </div>
     <div class="finish-wrapper">
       <button class="finish-btn">完成</button>
     </div>
-    <calendar v-model="calendarShow" @change="handleChange"></calendar>
+    <calendar v-model="birth" @change="handleBirth"></calendar>
+    <calendar v-model="meet" @change="handleMeet"></calendar>
+    <van-popup position="bottom" v-model="showVariety" class="select-variety">
+      <div class="confirms">
+        <ul>
+          <li class="cancel-btn" @click="showVariety = false">取消</li>
+          <li class="confirm-title">宠物种族</li>
+          <li class="confirm-btn" @click="showVariety = false; variety = tempVariety">确定</li>
+        </ul>
+      </div>
+      <div class="tag-wrapper">
+        <ul>
+          <li v-for="item in varieties">
+            <label>
+              <div class="avatar">
+                <img :src="[`../../../static/images/${item.avatar}@3x.png`]">
+              </div>
+              <div class="name">{{ item.name }}</div>
+              <input type="radio" name="variety" v-model="tempVariety" :value="item.name">
+              <div class="checked-wrapper">
+                <i class="checked"></i>
+              </div>
+            </label>
+          </li>
+        </ul>
+      </div>
+    </van-popup>
   </div>  
 </template>
 
 <script>
 import Calendar from '@/components/Calendar'
+import Varieties from '@/varieties.json'
 export default {
   name: 'add',
   components: {
@@ -51,17 +86,31 @@ export default {
     this.$store.commit('modNavbar', false)
     this.$store.commit('modClass', {inclass: 'slideInLeft', leaveclass: 'slideOutRight'})
     this.$store.commit('setTitle', '添加宠物')
+
   },
   methods: {
-    handleChange (date, formatDate) {
-      
+    handleBirth (date, formatDate) {
+      this.birthDay = formatDate
+    },
+    handleMeet (date, formatDate) {
+      this.meetDay = formatDate
     }
   },
   data () {
     return {
-      avatar: '../../../static/images/avatars/default.png',
+      avatar: '../../../static/images/avatars/pet_default.png',
       tempAvatar: 'default.png',
-      calendarShow: false
+      meet: false,
+      birth: false,
+      showVariety: false,
+      nickname: '',
+      motto: '',
+      gender: 'female',
+      birthDay: '',
+      meetDay: '',
+      tempVariety: '其他',
+      variety: '',
+      varieties: Varieties
     }
   }
 }
@@ -133,7 +182,7 @@ export default {
         color: #000;
         font-size: 18.5px;
         &.position {
-          color: #686868;
+          color: #9b9b9b;
           .right {
             display: inline-block;
             width: 7.5px;
@@ -163,7 +212,7 @@ export default {
               display: inline-block;
               width: 28px;
               height: 28px;
-              background: url('../../../static/images/female@3x.png') 100% e('/') 100% no-repeat;
+              background: url('../../../static/images/pet_gender@3x.png') 100% e('/') 100% no-repeat;
               transition: all .4s;
             }
             & + .gender_male {
@@ -174,7 +223,7 @@ export default {
               display: inline-block;
               width: 28px;
               height: 28px;
-              background: url('../../../static/images/male@3x.png') 100% e('/') 100% no-repeat;
+              background: url('../../../static/images/pet_gender@3x.png') 100% e('/') 100% no-repeat;
               transition: all .4s;
             }
             &:checked + .gender_female {
@@ -185,7 +234,7 @@ export default {
               display: inline-block;
               width: 28px;
               height: 28px;
-              background: url('../../../static/images/female_active@3x.png') 100% e('/') 100% no-repeat;
+              background: url('../../../static/images/pet_female@3x.png') 100% e('/') 100% no-repeat;
               transition: all .4s;
             }
             &:checked + .gender_male {
@@ -196,7 +245,7 @@ export default {
               display: inline-block;
               width: 28px;
               height: 28px;
-              background: url('../../../static/images/male_active@3x.png') 100% e('/') 100% no-repeat;
+              background: url('../../../static/images/pet_male@3x.png') 100% e('/') 100% no-repeat;
               transition: all .4s;
             }
           }
@@ -227,6 +276,123 @@ export default {
       }
       &:disabled {
         background: #d4d4d4;
+      }
+    }
+  }
+  .select-variety {
+    width: 100%;
+    height: 346.5px;
+    background-color: #fff;
+    .confirms {
+      width: 100%;
+      height: 44px;
+      background: #fff;
+      position: relative;
+      border-bottom: 1px solid #ccc;
+      ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        & > li {
+          font-size: 17.5px;
+          width: auto;
+          display: inline-block;
+          &.confirm-btn {
+            color: #1d5faa;
+            float: right;
+            padding: 8px 13px;
+          }
+          &.cancel-btn {
+            float: left;
+            color: #000;
+            padding: 8px 13px;
+          }
+          &.confirm-title {
+            display: inline-block;
+            font-size: 21.5px;
+            font-weight: bold;
+            text-align: center;
+            color: #000;
+            padding: 7px 0;
+            flex-grow: 1;
+          }
+        }
+      }
+    }
+    .tag-wrapper {
+      width: 100%;
+      height: 100%;
+      overflow-y: auto;
+      overflow-x: hidden;
+      background-color: #f5f5f5;
+      ul {
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        li {
+          width: 100%;
+          border-bottom: 1px solid #e6e6e6;
+          label {
+            width: 100%;
+            height: 50px;
+            display: flex;
+            box-sizing: border-box;
+            input[name=variety] {
+              display: none;
+              & + .checked-wrapper {
+                display: none;
+              }
+              &:checked + .checked-wrapper {
+                display: flex;
+              }
+            }
+            .avatar {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              box-sizing: border-box;
+              padding: 0 20px;
+              img {
+                width: 38px;
+                height: 38px;
+                border-radius: 50%;
+              }
+            }
+            .name {
+              display: flex;
+              flex-grow: 1;
+              justify-content: flex-start;
+              align-items: center;
+              font-size: 18.5px;
+              color: #686868;
+            }
+            .checked-wrapper {
+              width: 56px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              .checked {
+                display: inline-block;
+                width: 16px;
+                height: 11.5px;
+                background: url('../../../static/images/checked@3x.png') 100% e('/') 100% no-repeat;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .refresh-wrapper {
+    width: 100%;
+    height: 60px;
+    .refresh-btn {
+      font-size: 26.5px;
+      line-height: 60px;
+      display: inline-block;
+      color: #aaaaaa;
+      &:active {
+        color: #686868;
       }
     }
   }
